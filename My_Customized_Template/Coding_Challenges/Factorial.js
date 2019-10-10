ac3a40236c8199ea244331c422c107c274ed429c
treyhucks1179 <28307247+treyhucks1179@users.noreply.github.com>
2019-10-09 22:13:53 -0400

    oct-9-big-push

---
+++ My_Customized_Template/Coding_Challenges/Factorial.js
@@ -0,0 +1,33 @@
+﻿
+
+function factorial() {
+
+    alert("HERE 0 ");
+
+    $('#bmMessage').empty();
+
+    //Variables
+    var whiteSpan = "<span style='color: #fff; font-family:Times New Roman; font-size: 25px;'>";
+    var redSpan = "<span style='color: #ea1515; font-family:Times New Roman; font-size: 35px;'>";
+    var spanEnd = "</span>";
+    var brk = "<br /><br />";
+    var value;
+    var factorial;
+    var counter;
+
+    if ($.isNumeric($('#factNum').val()) && $('#factNum').val() !== "")
+    {
+        factorial = value;
+
+        for (i = value; i > 0; i--) {
+            factorial *= i;
+        }
+
+        $('#factMessage').append(whiteSpan + "Factorial is : " + factorial + spanEnd);
+    }
+    else
+    {
+        $('#factMessage').append("Enter a Number please");
+    }
+
+}
